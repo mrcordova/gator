d6b30cb1a89ba69cb87e6e528e62c3394e6af4eb
mrcordova <46950394+mrcordova@users.noreply.github.com>
2025-02-23 12:14:21 -0800

    list users cmd

--- handler_user.go
+++ handler_user.go
@@ -8,6 +8,20 @@ import (
 	"github.com/google/uuid"
 	"github.com/mrcordova/gator/internal/database"
 )
+func handlerListUsers(s *state, cmd command) error {
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't list users: %w", err)
+	}
+	for _, user := range users {
+		if user.Name == s.cfg.CurrentUserName {
+			fmt.Printf("* %v (current)\n", user.Name)
+			continue
+		}
+		fmt.Printf("* %v\n", user.Name)
+	}
+	return nil
+}
 
 func handlerRegister(s *state, cmd command) error {
 	if len(cmd.Args) != 1 {
