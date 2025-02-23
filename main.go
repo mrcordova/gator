11a666094b9046bb8e9f21cf4ba1f05bb9980263
mrcordova <46950394+mrcordova@users.noreply.github.com>
2025-02-22 19:32:56 -0800

    create command config

--- main.go
+++ main.go
@@ -1,28 +1,49 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"os"
 
 	"github.com/mrcordova/gator/internal/config"
 )
 
 
-func main()  {
-cfg, err := config.Read()
-	if err != nil {
-		log.Fatalf("error reading config: %v", err)
-	}
-	fmt.Printf("Read config: %+v\n", cfg)
-
-	err = cfg.SetUser("noah")
-	if err != nil {
-		log.Fatalf("Set User failed: %v", err )
-	}
-
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatalf("error reading config: %v", err)
-	}
-	fmt.Printf("Read config again: %+v\n", cfg)
+type state struct {
+	cfg *config.Config
 }
+
+
+
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+
+	programState := &state{
+		cfg: &cfg,
+	}
+
+	cmds := commands{
+		registeredCommands: make(map[string]func(*state, command) error),
+	}
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: cli <command> [args...]")
+		return
+	}
+
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
+
+	err = cmds.run(programState, command{Name: cmdName, Args: cmdArgs})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+
+
+
